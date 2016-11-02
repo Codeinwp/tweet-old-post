c83ec8595f07ac2c287346082a9391ec47726c3a
selu91 <cristea.marius.alexandru@gmail.com>
2016-11-02 18:18:05 +0200

    bump version

--- tweet-old-post.php
+++ tweet-old-post.php
@@ -4,7 +4,7 @@
 #     Plugin URI: https://revive.social/
 #     Description: Wordpress plugin that helps you to keeps your old posts alive by sharing them and driving more traffic to them from twitter/facebook or linkedin. It also helps you to promote your content. You can set time and no of posts to share to drive more traffic.For questions, comments, or feature requests, <a href="http://revive.social/support/?utm_source=plugindesc&utm_medium=announce&utm_campaign=top">contact </a> us!
 #     Author: revive.social
-#     Version: 7.4.0
+#     Version: 7.4.5
 #     Author URI: https://revive.social/
 #	  Text Domain: tweet-old-post
 #	  Domain Path: /languages
