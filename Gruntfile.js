d12e0b4ba36c4c1025085bf1ccdc2602223d86b4
selu91 <cristea.marius.alexandru@gmail.com>
2017-08-14 16:24:31 +0300

    Removed redundant code in grunfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,5 @@ module.exports = function (grunt) {
     var loader = require( 'load-project-config' ),
         config = require( 'grunt-plugin-fleet' );
     config = config();
-    config.files.php.push( '!inc/PhpFormBuilder.php' );
-    config.files.php.push( '!mailin.php' );
     loader( grunt, config ).init();
 };
