b75df431352ea46a7b15bdec33794cb9320e68f4
Bogdan Preda <bogdan.preda@themeisle.com>
2017-08-17 16:50:41 +0300

    WIP: Cleanup structure

--- includes/admin/class-rop-admin.php
+++ includes/admin/class-rop-admin.php
@@ -73,7 +73,7 @@ class Rop_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/rop-admin.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, ROP_LITE_URL . 'assets/css/rop-admin.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +96,7 @@ class Rop_Admin {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/rop-admin.js', array( 'jquery' ), $this->version, false );
+		wp_enqueue_script( $this->plugin_name, ROP_LITE_URL . 'assets/js/rop.js', array( 'jquery' ), $this->version, false );
 
 	}
 
