3a302f233b71176d891be11c6cec6951a4523f92
Bogdan Preda <bogdan.preda@themeisle.com>
2018-01-09 14:22:00 +0200

    WIP: Started the upgrade hellper for to use in plugin activation

---
+++ includes/admin/helpers/class-rop-db-upgrade.php
@@ -0,0 +1,41 @@
+<?php
+class Rop_Db_Upgrade {
+	public function __construct() {
+
+	}
+
+
+	/**
+	 * Method to check if upgrade is required.
+	 *
+	 * @since   8.0.0
+	 * @access  public
+	 * @return bool
+	 */
+	public function is_upgrade_required() {
+
+	}
+
+
+	/**
+	 * Method to do the required upgrade.
+	 *
+	 * @since   8.0.0
+	 * @access  public
+	 * @return bool
+	 */
+	public function do_upgrade() {
+
+	}
+
+	/**
+	 * Method to do a revert for an upgrade action.
+	 *
+	 * @since   8.0.0
+	 * @access  public
+	 * @return bool
+	 */
+	public function revert() {
+
+	}
+}
