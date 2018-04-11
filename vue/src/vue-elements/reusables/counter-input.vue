3a9b233af93a6e4ae24c99a0c983a4316d95d125
selu91 <cristea.marius.alexandru@gmail.com>
2018-04-11 15:09:12 +0300

    Fix issue with counter input update.

--- vue/src/vue-elements/reusables/counter-input.vue
+++ vue/src/vue-elements/reusables/counter-input.vue
@@ -1,8 +1,10 @@
 <template>
 	<div class="input-group rop-counter-group">
-		<input class="form-input rop-counter" type="text" v-model="inputValueC" :id="id"  >
-		<button class="btn input-group-btn increment-btn up" @mousedown="isPressed('up')" @mouseup="isReleased('up')"><i class="fa fa-fw fa-caret-up"></i></button>
-		<button class="btn input-group-btn increment-btn down" @mousedown="isPressed('down')" @mouseup="isReleased('down')"><i class="fa fa-fw fa-caret-down"></i></button>
+		<input class="form-input rop-counter" type="text" v-model="inputValueC" :id="id">
+		<button class="btn input-group-btn increment-btn up" @mousedown="isPressed('up')" @mouseup="isReleased('up')"><i
+				class="fa fa-fw fa-caret-up"></i></button>
+		<button class="btn input-group-btn increment-btn down" @mousedown="isPressed('down')"
+		        @mouseup="isReleased('down')"><i class="fa fa-fw fa-caret-down"></i></button>
 	</div>
 </template>
 
@@ -43,52 +45,54 @@
 		computed: {
 			inputValueC: {
 				get: function () {
-					return this.value
+					return this.value;
 				},
-				set: function ( newValue ) {
-					this.inputValue = parseFloat( newValue )
-					this.$emit( 'update:value', this.inputValue )
+				set: function (newValue) {
+					this.inputValue = parseFloat(newValue)
+					this.$emit('update:value', this.inputValue)
 				}
 
 			}
 		},
 		methods: {
-			updateInput () {
-				this.inputValue = this.value
+			updateInput() {
+				this.inputValue = this.value.toString();
+				this.inputValue = parseFloat(this.inputValue);
 				let now = new Date()
-				let secondsPassed = parseInt( ( now.getTime() - this.pressStartTime.getTime() ) / 1000 )
+				let secondsPassed = parseInt((now.getTime() - this.pressStartTime.getTime()) / 1000)
 				let increment = secondsPassed
-				if ( secondsPassed === 0 ) increment = 1
-
-				if ( this.incrementUp === 1 ) {
+				if (secondsPassed === 0) increment = 1
+				increment = parseInt(increment);
+				if (this.incrementUp === 1) {
 					this.inputValue += increment
-					if ( this.inputValue > this.maxVal && this.maxVal !== 0 ) this.inputValue = this.maxVal
+					if (this.inputValue > this.maxVal && this.maxVal !== 0) this.inputValue = this.maxVal
 				}
-				if ( this.incrementDown === 1 ) {
+				if (this.incrementDown === 1) {
 					this.inputValue -= increment
-					if ( this.inputValue < 0 && this.allowNegative === false ) this.inputValue = 0
-					if ( this.inputValue < this.minVal ) this.inputValue = this.minVal
+					if (this.inputValue < 0 && this.allowNegative === false) this.inputValue = 0
+					if (this.inputValue < this.minVal) this.inputValue = this.minVal
 				}
-				this.$emit( 'update:value', this.inputValue )
+				this.inputValue = parseFloat(this.inputValue.toFixed(1));
+				this.$emit('update:value', this.inputValue)
 			},
-			isPressed ( type ) {
-				if ( type === 'up' ) {
+			isPressed(type) {
+				if (type === 'up') {
 					this.incrementUp = 1
 				} else {
 					this.incrementDown = 1
 				}
 				this.pressStartTime = new Date()
 				this.updateInput()
-				intervalID = setInterval( this.updateInput, 250 )
+				intervalID = setInterval(this.updateInput, 250)
 			},
-			isReleased ( type ) {
-				if ( type === 'up' ) {
+			isReleased(type) {
+				if (type === 'up') {
 					this.incrementUp = 0
 				} else {
 					this.incrementDown = 0
 				}
 				this.pressStartTime = null
-				clearInterval( intervalID )
+				clearInterval(intervalID)
 			}
 		}
 	}
@@ -99,6 +103,7 @@
 		position: relative;
 		width: 100%;
 	}
+	
 	#rop_core .btn.increment-btn {
 		position: absolute;
 		right: 0;
@@ -110,15 +115,21 @@
 		color: #ababab;
 		border-color: #ababab;
 	}
+	
 	#rop_core .btn.increment-btn:hover, #rop_core .btn.increment-btn:active, #rop_core .btn.increment-btn:focus {
 		background-color: #00a6e3;
 		color: #fff;
 		border-color: #00a6e3;
 	}
-
-	#rop_core .btn.increment-btn.up { top: 0; }
-	#rop_core .btn.increment-btn.down { bottom: 0; }
-
+	
+	#rop_core .btn.increment-btn.up {
+		top: 0;
+	}
+	
+	#rop_core .btn.increment-btn.down {
+		bottom: 0;
+	}
+	
 	input.rop-counter::-webkit-inner-spin-button {
 		display: none;
 	}
