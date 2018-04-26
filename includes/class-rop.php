2988bf66224ae43cb54b085d009c8ae9a99a000b
Marius Cristea <marius.cristea@vertistudio.com>
2018-04-27 01:56:17 +0300

    Bump version.

--- includes/class-rop.php
+++ includes/class-rop.php
@@ -68,7 +68,7 @@ class Rop {
 	public function __construct() {
 
 		$this->plugin_name = 'rop';
-		$this->version     = '8.0.0';
+		$this->version     = '8.0.1';
 
 		$this->load_dependencies();
 		$this->set_locale();
