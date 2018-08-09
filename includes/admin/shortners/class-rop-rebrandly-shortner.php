90f85897597fc9f2547b737334a13b1e2ad9518d
Ashish Ravi <contactashish13@gmail.com>
2018-08-09 09:08:16 +0530

    use title and domain

--- includes/admin/shortners/class-rop-rebrandly-shortner.php
+++ includes/admin/shortners/class-rop-rebrandly-shortner.php
@@ -28,6 +28,7 @@ class Rop_Rebrandly_Shortner extends Rop_Url_Shortner_Abstract {
 		$this->service_name = 'rebrand.ly';
 		$this->credentials  = array(
 			'key'  => '',
+			'domain'  => '',
 		);
 	}
 
@@ -41,16 +42,28 @@ class Rop_Rebrandly_Shortner extends Rop_Url_Shortner_Abstract {
 	 * @return string
 	 */
 	public function shorten_url( $url ) {
+		$post_id    = url_to_postid( $url );
+		$title      = '';
+		if ( $post_id ) {
+			$title  = get_the_title( $post_id );
+		}
+
 		$response = $this->callAPI(
 			'https://api.rebrandly.com/v1/links',
 			array( 'method' => 'json', 'json' => true ),
-			array( 'destination' => $url ),
+			array( 'destination' => $url, 'title' => $title, 'domain' => array( 'id' => $this->credentials['domain'] ) ),
 			array( 'apikey' => $this->credentials['key'], 'Content-Type' => 'application/json' )
 		);
 
 		$shortURL = $url;
-		if ( intval( $response['error'] ) == 200 ) {
-			$shortURL = $response['response']['shortUrl'];
+		if ( intval( $response['error'] ) === 200 ) {
+			if ( ! array_key_exists( 'httpCode', $response['response'] ) ) {
+				$shortURL = $response['response']['shortUrl'];
+			} else {
+				$this->error->throw_exception( 'Error', "Error from {$this->service_name} " . $response['response']['message'] );
+			}
+		} else {
+			$this->error->throw_exception( 'Error', "Error from {$this->service_name} " . $response['error'] );
 		}
 		return $shortURL;
 	}
