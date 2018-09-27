e5cc0821e936fcfe9754ba4d04ad729a48a628cc
selu91 <cristea.marius.alexandru@gmail.com>
2018-09-27 12:37:04 +0300

    adds check for empty remaints of accounts.

--- includes/admin/models/class-rop-services-model.php
+++ includes/admin/models/class-rop-services-model.php
@@ -303,7 +303,9 @@ class Rop_Services_Model extends Rop_Model_Abstract {
 			},
 			$accounts
 		);
-
+		$accounts = array_filter( $accounts, function ( $account ) {
+			return ! empty( $account );
+		} );
 		return wp_parse_args( $accounts, array() );
 	}
 
