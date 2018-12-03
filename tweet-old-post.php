099268f6e7bb6a2cc1cc6d9d7130447b92d5eeb5
TheLoneDeveloper <uriahs.victor@gmail.com>
2018-12-03 09:07:03 -0400

    changed tested up to wp version

--- tweet-old-post.php
+++ tweet-old-post.php
@@ -20,12 +20,12 @@
  * Author:            revive.social
  * Author URI:        https://revive.social/
  * Requires at least: 3.5
- * Tested up to:      4.8
+ * Tested up to:      5.0
  * Stable tag:        trunk
  * WordPress Available:  yes
  * Pro Slug:          tweet-old-post-pro
  * Requires License:    no
- * Requires PHP: 5.4
+ * Requires PHP: 5.6
  * License:           GPLv2 or later
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: tweet-old-post
