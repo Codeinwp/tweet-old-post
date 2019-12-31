8661e57e7cc18ae241c4145f3ff34dab1d9637e9
Uriahs Victor <uriahs.victor@gmail.com>
2019-12-31 16:21:42 -0400

    new version

--- includes/class-rop.php
+++ includes/class-rop.php
@@ -68,7 +68,7 @@ class Rop {
 	public function __construct() {
 
 		$this->plugin_name = 'rop';
-		$this->version     = '8.5.1';
+		$this->version     = '8.5.2';
 
 		$this->load_dependencies();
 		$this->set_locale();
