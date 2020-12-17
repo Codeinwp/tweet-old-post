db02d8e0848ac3879c186d17290cb3249ab3db37
UVLabs <uriahs.victor@gmail.com>
2020-12-17 14:07:36 -0400

    grunt local

--- tweet-old-post.php
+++ tweet-old-post.php
@@ -167,7 +167,7 @@ function run_rop() {
 	if ( function_exists( 'wp_get_environment_type' ) ) {
 		if ( wp_get_environment_type() !== 'production' ) {
 			define( 'ROP_DEBUG', true );
-		}else{
+		} else {
 			define( 'ROP_DEBUG', false );
 		}
 	} else {
