cfeb8bc0ba02924094873a4526a1ccfa12ccad57
UVLabs <uriahs.victor@gmail.com>
2021-05-25 14:34:17 -0400

    release: new version ### Fix - Test

--- includes/class-rop.php
+++ includes/class-rop.php
@@ -68,7 +68,7 @@ class Rop {
 	public function __construct() {
 
 		$this->plugin_name = 'rop';
-		$this->version     = '9.1.0';
+		$this->version     = '10.0.0';
 
 		$this->load_dependencies();
 		$this->set_locale();
