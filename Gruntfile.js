c048ddd65707d7f67c93d84ab516d15648b3b089
UVLabs <uriahs.victor@gmail.com>
2021-05-31 15:21:18 -0400

    made gruntfile changes, added needed packages to package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,49 @@
-/* jshint node:true */
-/* global require, grunt */
-module.exports = function ( grunt ) {
-	'use strict'
-
-	var loader = require( 'load-project-config' )
-	var config = require( 'grunt-plugin-fleet' )
-	config     = config()
-	loader( grunt, config ).init()
-}
+/**
+ * Grunt File
+ *
+ * @package tweet-old-post
+ */
+ module.exports = function (grunt) {
+	grunt.initConfig(
+		{
+			wp_readme_to_markdown: {
+				files: {
+					'readme.md': 'readme.txt'
+				},
+			},
+			version: {
+				project: {
+					src: [
+					'package.json'
+					]
+				},
+				style: {
+					options: {
+						prefix: 'Version\\:\.*\\s'
+					},
+					src: [
+					'tweet-old-post.php',
+					]
+				},
+				functions: {
+					options: {
+						prefix: 'version\\s+=\\s+[\'"]'
+					},
+					src: [
+					'includes/class-rop.php',
+					]
+				},
+				constants: {
+					options: {
+						prefix: 'ROP_LITE_VERSION\'\,\\s+\''
+					},
+					src: [
+					'tweet-old-post.php',
+					]
+				}
+			}
+		}
+	);
+	grunt.loadNpmTasks( 'grunt-version' );
+	grunt.loadNpmTasks( 'grunt-wp-readme-to-markdown' );
+};
