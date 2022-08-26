5331cca80e5f6bc89c196f17f5589934e64787d9
UVLabs <uriahs.victor@gmail.com>
2022-08-26 12:06:15 -0400

    chore: use getMessage() on exceptions when service fails to build instead of getTrace()

--- includes/admin/class-rop-admin.php
+++ includes/admin/class-rop-admin.php
@@ -1049,7 +1049,7 @@ class Rop_Admin {
 					}
 				} catch ( Exception $exception ) {
 					$error_message = sprintf( Rop_I18n::get_labels( 'accounts.service_error' ), $account_data['service'] );
-					$logger->alert_error( $error_message . ' Error: ' . print_r( $exception->getTrace(), true ) );
+					$logger->alert_error( $error_message . ' Error: ' . print_r( $exception->getMessage(), true ) );
 				}
 			}
 		}
