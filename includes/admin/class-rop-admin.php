bff37290e4bf77cec69e5ade10e96651d957d7ee
Soare Robert Daniel (Mac 2023) <soare.robert.daniel@protonmail.com>
2024-05-22 10:53:10 +0300

    chore: php style

--- includes/admin/class-rop-admin.php
+++ includes/admin/class-rop-admin.php
@@ -1789,16 +1789,15 @@ HTML;
 	/**
 	 * Get the data used for the survey.
 	 *
-	 * @return array
-	 * @see survey.js
+	 * @return array The survey metadata.
 	 */
 	public function get_survey_metadata() {
 		$license_data = get_option( 'feedzy_rss_feeds_pro_license_data', array() );
-		$attributes = array();
-		$user_id = 'rop_' . ( ! empty( $license_data->key ) ? $license_data->key : preg_replace( '/[^\w\d]*/', '', get_site_url() ) ); // Use a normalized version of the site URL as a user ID for free users.
+		$attributes   = array();
+		$user_id      = 'rop_' . ( ! empty( $license_data->key ) ? $license_data->key : preg_replace( '/[^\w\d]*/', '', get_site_url() ) ); // Use a normalized version of the site URL as a user ID for free users.
 
 		$days_since_install = round( ( time() - get_option( 'rop_first_install_date', 0 ) ) / DAY_IN_SECONDS );
-		$install_category = 0; // Normalized value.
+		$install_category   = 0; // Normalized value.
 		if ( 0 === $days_since_install || 1 === $days_since_install ) {
 			$install_category = 0;
 		} elseif ( 1 < $days_since_install && 8 > $days_since_install ) {
@@ -1824,7 +1823,7 @@ HTML;
 		}
 
 		return array(
-			'userId' => $user_id,
+			'userId'     => $user_id,
 			'attributes' => $attributes,
 		);
 	}
@@ -1832,7 +1831,7 @@ HTML;
 	/**
 	 * Register the survey script.
 	 *
-	 * It does register if we are in CI environment.
+	 * It does not register if we are in a testing environment.
 	 *
 	 * @return void
 	 */
