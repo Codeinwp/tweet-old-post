b82099522ac10079d0b3f8501f50beadd8f55126
girishpanchal30 <girish@krishaweb.com>
2025-01-08 14:27:46 +0530

    feat: add support in fb old method

--- includes/admin/services/class-rop-facebook-service.php
+++ includes/admin/services/class-rop-facebook-service.php
@@ -726,7 +726,16 @@ class Rop_Facebook_Service extends Rop_Services_Abstract {
 					$this->rop_fb_scrape_url( $posting_type, $post_id, $token );
 				}
 
-				$api->post( $path, $new_post, $token );
+				$response   = $api->post( $path, $new_post, $token );
+				$fb_post_id = $response->getGraphNode()->getField( 'id' );
+				$fb_helper  = new ROP_Pro_Facebook_Helper();
+				$fb_helper->share_as_first_comment(
+					$fb_post_id,
+					array(
+						'message'      => $share_link_text,
+						'access_token' => $token,
+					)
+				);
 
 				return true;
 			} catch ( Facebook\Exceptions\FacebookResponseException $e ) {
@@ -750,8 +759,16 @@ class Rop_Facebook_Service extends Rop_Services_Abstract {
 					}
 
 					try {
-						$api->post( $path, $new_post, $token );
-
+						$response   = $api->post( $path, $new_post, $token );
+						$fb_post_id = $response->getGraphNode()->getField( 'id' );
+						$fb_helper  = new ROP_Pro_Facebook_Helper();
+						$fb_helper->share_as_first_comment(
+							$fb_post_id,
+							array(
+								'message'      => $share_link_text,
+								'access_token' => $token,
+							)
+						);
 						return true;
 					} catch ( Facebook\Exceptions\FacebookResponseException $e ) {
 						$this->logger->alert_error( 'Unable to share post for facebook. (FacebookResponseException) Error: ' . $e->getMessage() );
@@ -907,6 +924,14 @@ class Rop_Facebook_Service extends Rop_Services_Abstract {
 					}
 
 					if ( ! empty( $body['id'] ) ) {
+						$fb_helper = new ROP_Pro_Facebook_Helper();
+						$fb_helper->share_as_first_comment(
+							$body['id'],
+							array(
+								'message'      => $share_link_text,
+								'access_token' => $token,
+							)
+						);
 						return true;
 					} elseif ( ! empty( $body['error']['message'] ) ) {
 						$this->logger->alert_error( 'Error Posting to Facebook: ' . $body['error']['message'] );
