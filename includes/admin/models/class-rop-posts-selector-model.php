d1d09d6b222fa6a5cc73c9f509c9603b6d476cb2
Hardeep Asrani <hardeepasrani@gmail.com>
2025-07-15 16:49:00 +0530

    fix: publish now posting previous posts

--- includes/admin/models/class-rop-posts-selector-model.php
+++ includes/admin/models/class-rop-posts-selector-model.php
@@ -734,6 +734,10 @@ class Rop_Posts_Selector_Model extends Rop_Model_Abstract {
 						'key'   => 'rop_publish_now',
 						'value' => 'yes',
 					),
+					array( // we didn't want to add this check, it's redundant but it seems in previous version delete_post_meta was not working properly.
+						'key'   => 'rop_publish_now_status',
+						'value' => 'queued',
+					),
 				),
 				'numberposts' => 300,
 				'orderby'     => 'modified',
@@ -748,7 +752,7 @@ class Rop_Posts_Selector_Model extends Rop_Model_Abstract {
 			while ( $query->have_posts() ) {
 				$query->the_post();
 				$posts[] = $query->post;
-				// delete the meta so that when the post loads again after publishing, the checkboxes are cleared.
+				// update the meta so that when the post loads again after publishing, the checkboxes are cleared.
 				update_post_meta( $query->post, 'rop_publish_now', 'no' );
 			}
 		}
